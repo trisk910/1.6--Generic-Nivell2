19f46c6c777d4878657aeae98fa73201124817f5
Marc Bernabeu Rodriguez <marc_b_r@hotmail.com>
2024-10-30 13:20:05 +0100

    Nivell1 fet

---
+++ 1.6--Generic-Nivellx/src/main/java/org/example/Level1/Bussiness/Persona.java
@@ -0,0 +1,42 @@
+package org.example.Level1.Bussiness;
+
+public class Persona {
+    private String nom;
+    private String cognom;
+    private int edat;
+
+    public Persona(String nom, String cognom, int edat) {
+        this.nom = nom;
+        this.cognom = cognom;
+        this.edat = edat;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCognom() {
+        return cognom;
+    }
+
+    public void setCognom(String cognom) {
+        this.cognom = cognom;
+    }
+
+    public int getEdat() {
+        return edat;
+    }
+
+    public void setEdat(int edat) {
+        this.edat = edat;
+    }
+    @Override
+    public String toString() {
+        return "Persona{" + "nom='" + nom + '\'' + ", cognom='" + cognom + '\'' +
+                ", edat=" + edat +'}';
+    }
+}
---
+++ 1.6--Generic-Nivellx/src/main/java/org/example/Level1/MainLevel1.java
@@ -0,0 +1,28 @@
+package org.example.Level1;
+
+import org.example.Level1.Bussiness.GenericMethods;
+import org.example.Level1.Bussiness.NoGenericMethods;
+import org.example.Level1.Bussiness.Persona;
+
+public class MainLevel1 {
+    public static void main(String[] args) {
+        System.out.println("----------Ex1----------");
+        NoGenericMethods obj1 = new NoGenericMethods(1, 2, 3);
+        NoGenericMethods obj2 = new NoGenericMethods(3, 1, 2);
+        NoGenericMethods obj3 = new NoGenericMethods(2, 3, 1);
+
+        System.out.println("Objecte 1: " + obj1.getValue1() + ", " + obj1.getValue2() + ", " + obj1.getValue3());
+        System.out.println("Objecte 2: " + obj2.getValue1() + ", " + obj2.getValue2() + ", " + obj2.getValue3());
+        System.out.println("Objecte 3: " + obj3.getValue1() + ", " + obj3.getValue2() + ", " + obj3.getValue3());
+
+        System.out.println("----------Ex2----------");
+        Persona persona = new Persona("Pepito", "Palotes", 50);
+        String text = "Hola";
+        int number = 22;
+
+        GenericMethods gm = new GenericMethods();
+        gm.printArguments(persona, text, number);
+        gm.printArguments(text, number, persona);
+        gm.printArguments(number, persona, text);
+    }
+}
